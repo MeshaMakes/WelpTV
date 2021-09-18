a89c1e08a7b34d6da06f34882e0ea80ea975ff93
Cesar Diaz <gfinderprod@gmail.com>
2021-09-18 10:04:38 -0400

    added series card component

--- welptv/src/App.js
+++ welptv/src/App.js
@@ -1,11 +1,14 @@
 import "./Stylesheets/app.css";
 import Navbar from"./Components/navbar";
+import SeriesCard from"./Components/seriesCard";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <div className="home"></div>
+      <div className="home">
+        <SeriesCard></SeriesCard>
+      </div>
       <div className="series"></div>
     </div>
   );
---
+++ welptv/src/components/seriesCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../Stylesheets/seriesCard.css";
+
+const SeriesCard = () => {
+    return (
+        <div className="thumbnail">
+
+        </div>
+    );
+}
+
+export default SeriesCard;
