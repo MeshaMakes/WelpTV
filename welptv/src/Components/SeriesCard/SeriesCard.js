5ac60e60e044900deb07b5656b9a0d3a009b19e9
Cesar Diaz <gfinderprod@gmail.com>
2021-09-18 19:21:35 -0400

    updated SeriesCard componenet

--- welptv/src/Components/SeriesCard/SeriesCard.js
+++ welptv/src/Components/SeriesCard/SeriesCard.js
@@ -7,23 +7,26 @@ const SeriesCard = (props) => {
         return (
             <div className="thumbnail" style={{backgroundImage: props.data.image}}>
                 <div className="overlay" style={{backgroundColor: props.data.color}}></div>
-                <h1 className="fadeTitle">THE TOP 10</h1>
+                <h1 className="topTitle">THE TOP 10</h1>
                 <h1 className="subTitle">{props.data.title}</h1>
             </div>
         );
     }else if(props.type === "poster"){
         return (
             <div className="poster">
-                <img className="image" src={props.data.image} alt={props.data.title}/>
-                <h1 className="posterTitle">{props.data.title}</h1>
+            <img className="posterImage" src={props.data.image} alt={props.data.title}/>
+                <h1 className="title">{props.data.title}</h1>
                 <h1 className="subTitle">{props.data.season}</h1>
             </div>
         );
     }else{
         return (
             <div className="ticket">
-                <h1 className="fadeTitle">THE TOP 10</h1>
-                <h1 className="title">Jujustsu Kaisen</h1>
+                <img className="ticketImage" src={props.data.image} alt={props.data.title}/>
+                <div className="details">
+                    <h1 className="title">{props.data.title}</h1>
+                    <h1 className="subTitle">{props.data.season}</h1>
+                </div>
             </div>
         );
     }
