2d1c38593880b78cd786322de81b84835bfe09dd
Cesar Diaz <gfinderprod@gmail.com>
2021-09-18 11:55:33 -0400

    adding thumnail series card

--- welptv/src/Components/SeriesCard/SeriesCard.js
+++ welptv/src/Components/SeriesCard/SeriesCard.js
@@ -4,7 +4,14 @@ import "./SeriesCard.css";
 const SeriesCard = () => {
     return (
         <div className="thumbnail">
-            <h1>TOP</h1>
+            <div className="overlay"></div>
+            <div className="tag">
+                <h1>TOP</h1>
+            </div>
+
+            <h1>Jujustsu Kaisen</h1>
+            <h1>Jujustsu Kaisen</h1>
+            <h1>Jujustsu Kaisen</h1>
         </div>
     );
 }
