f8e5b85e9efef3087f25904fe9b9b1061afc7680
Cesar Diaz <gfinderprod@gmail.com>
2021-09-18 14:24:15 -0400

    updated SeriesCard component

--- welptv/src/App.js
+++ welptv/src/App.js
@@ -3,12 +3,29 @@ import Navbar from "./Components/NavBar/NavBar";
 import SeriesCard from"./Components/SeriesCard/SeriesCard";
 
 function App() {
+
+  var data = {
+    title: "Jujustsu Kaisen",
+    season: "fall 2020",
+    color: "#0084ff30",
+    image: "https://i.pinimg.com/originals/ed/00/4b/ed004b904fad8a61094f993eab787e05.jpg",
+  };
+
+
   return (
     <div className="App">
       <Navbar/>
+
       <div className="home">
-        <SeriesCard></SeriesCard>
+
+        <div style={{display: "flex", alignItems: "center"}}>
+          <SeriesCard type="thumbnail" data={data}></SeriesCard>
+          <SeriesCard type="poster" data={data}></SeriesCard>
+          <SeriesCard type="ticket" data={data}></SeriesCard>
+        </div>
+
       </div>
+      
       <div className="series"></div>
     </div>
   );
--- welptv/src/Components/SeriesCard/SeriesCard.js
+++ welptv/src/Components/SeriesCard/SeriesCard.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import "./SeriesCard.css";
 
-const SeriesCard = () => {
-    return (
-        <div className="thumbnail">
-            <div className="overlay"></div>
-            <div className="tag">
-                <h1>TOP</h1>
+const SeriesCard = (props) => {
+    if(props.type === "thumbnail"){
+        console.log(props);
+        return (
+            <div className="thumbnail" style={{backgroundImage: props.data.image}}>
+                <div className="overlay" style={{backgroundColor: props.data.color}}></div>
+                <h1 className="fadeTitle">THE TOP 10</h1>
+                <h1 className="subTitle">{props.data.title}</h1>
             </div>
-
-            <h1>Jujustsu Kaisen</h1>
-            <h1>Jujustsu Kaisen</h1>
-            <h1>Jujustsu Kaisen</h1>
-        </div>
-    );
+        );
+    }else if(props.type === "poster"){
+        return (
+            <div className="poster">
+                <img className="image" src={props.data.image} alt={props.data.title}/>
+                <h1 className="posterTitle">{props.data.title}</h1>
+                <h1 className="subTitle">{props.data.season}</h1>
+            </div>
+        );
+    }else{
+        return (
+            <div className="ticket">
+                <h1 className="fadeTitle">THE TOP 10</h1>
+                <h1 className="title">Jujustsu Kaisen</h1>
+            </div>
+        );
+    }
 }
 
 export default SeriesCard;
